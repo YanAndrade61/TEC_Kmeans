cbe381ca3f81efb958085613e1b8ecae056aa39e
yan <yanccomp@gmail.com>
2022-10-26 13:23:12 -0300

    Final project

--- kmeans.py
+++ kmeans.py
@@ -1,18 +1,26 @@
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class kmeans:
 
     def __init__(self, data: list, k : int = 2):
         self.k = k
         self.data = data
-        self.centroids = self.first_centroids()
+        self.centroids = self.first_centroids1()
 
-    def first_centroids(self):
+    def first_centroids1(self):
+
+        centroids = self.data[np.random.randint(self.data.shape[0], size=self.k), :].copy()
+
+        return centroids
+
+    def first_centroids2(self):
         centroids = list()
+        
         for i in range(self.k):
-            c = [np.random.randint(p) for p in self.data.max(axis=0)]
+            c = [np.random.randint(min,max) for min,max in zip(self.data.min(axis=0),self.data.max(axis=0))]
             centroids.append(c)
+
         return centroids
     
     def recalculate_centroids(self,clusters):
@@ -38,19 +46,24 @@ class kmeans:
 
         for i in range(it):
             clusters = self.generate_clusters()
-            self.recalculate_centroids()
+            self.recalculate_centroids(clusters)
         
+        self.view_clusters(clusters)
+
         return clusters
 
-    def view_clusters(self):
+    def view_clusters(self,clusters):
+        #Tratar o dcit antes do plot
+        #Seaborn
+        color = [(np.random.random(),np.random.random(),np.random.random(),1.0) for i in range(self.k)]
+        plt.figure()
         
-        cores = [(np.random.random(),np.random.random(),np.random.random(),1.0) for i in range(2)]
-        
-        for i in self.k:
+        for i in range(self.k):
+            if len(clusters[i]) > 0:
+                coord_x = np.array(clusters[i])[:,0]
+                coord_y = np.array(clusters[i])[:,1]
+                plt.plot(coord_x,coord_y,"o",color=color[i])
 
-            pass
-
-def main():
-    data = pd.read_csv("s02.txt")
-    data = data.to_numpy()
-    print("oi",data)
+        plt.xscale
+        plt.savefig("clusters.png")
+        plt.show()
---
+++ main.py
@@ -0,0 +1,4 @@
+def main():
+    data = pd.read_csv("s02.txt")
+    data = data.to_numpy()
+    print("oi",data)
