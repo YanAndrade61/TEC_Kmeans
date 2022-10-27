6c2c5e14eb0fe4f2ac663d4143d25de11ff75152
yan <yanccomp@gmail.com>
2022-10-27 00:32:29 -0300

    Beautiful plot

--- kmeans.py
+++ kmeans.py
@@ -1,31 +1,20 @@
+from dataclasses import replace
+import string
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-class kmeans:
+class Kmeans:
 
     def __init__(self, data: list, k : int = 2):
-        self.k = k
         self.data = data
-        self.centroids = self.first_centroids1()
+        self.k = k
 
-    def first_centroids1(self):
-
-        centroids = self.data[np.random.randint(self.data.shape[0], size=self.k), :].copy()
-
-        return centroids
-
-    def first_centroids2(self):
-        centroids = list()
-        
-        for i in range(self.k):
-            c = [np.random.randint(min,max) for min,max in zip(self.data.min(axis=0),self.data.max(axis=0))]
-            centroids.append(c)
-
-        return centroids
-    
     def recalculate_centroids(self,clusters):
-        for i in range(self.k):
-            self.centroids[i] = np.mean(clusters[i],axis=0)
+        
+        centroids = [np.mean(clusters[i],axis=0) for i in range(self.k)]
+        
+        return centroids
 
     def generate_clusters(self):
 
@@ -42,28 +31,45 @@ class kmeans:
 
         return clusters
     
-    def simulate(self,it):
+    def simulate(self, first_centroids : str = "random_point", it: int = 1000, error_stop: float = 0.001):
+
+        if first_centroids == "random_point":
+            self.centroids = self.data[np.random.randint(self.data.shape[0], size=self.k), :].copy()
+        elif first_centroids == "random_vector":
+            self.centroids = [np.random.randint(min,max) for min,max in zip(self.data.min(axis=0),self.data.max(axis=0))]
+        else:
+            print("Please enter a valid strategie: random_point or random_vector")
+            return
 
         for i in range(it):
+            stop = 0
             clusters = self.generate_clusters()
-            self.recalculate_centroids(clusters)
-        
-        self.view_clusters(clusters)
+            centroids = self.recalculate_centroids(clusters)
+    
+            for c_new,c_past in zip(centroids,self.centroids):
+                if np.linalg.norm(c_new-c_past) < error_stop: stop += 1
+
+            if stop == self.k: break
+
+            self.centroids = centroids
+
+        self.clusters = clusters
 
         return clusters
 
-    def view_clusters(self,clusters):
-        #Tratar o dcit antes do plot
-        #Seaborn
-        color = [(np.random.random(),np.random.random(),np.random.random(),1.0) for i in range(self.k)]
+    def view_clusters(self,path_save : str):
+        colors = cm.rainbow(np.linspace(0, 1, 5))
         plt.figure()
-        
         for i in range(self.k):
-            if len(clusters[i]) > 0:
-                coord_x = np.array(clusters[i])[:,0]
-                coord_y = np.array(clusters[i])[:,1]
-                plt.plot(coord_x,coord_y,"o",color=color[i])
 
-        plt.xscale
-        plt.savefig("clusters.png")
+            if len(self.clusters[i]) > 0:
+                coord_x = np.array(self.clusters[i])[:,0]
+                coord_y = np.array(self.clusters[i])[:,1]
+                plt.plot(coord_x,coord_y,"o",color=colors[i])
+                plt.plot(self.centroids[i][0],self.centroids[i][1],"o",markersize=10,
+                        markeredgecolor="black",color=colors[i],label=f"Centroid {i}")
+        
+        plt.title(f"Kmeans with {self.k} clusters")
+        plt.legend()
+        plt.savefig(path_save)
         plt.show()
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from kmeans import kmeans
+import pandas as pd
+
 def main():
+    
     data = pd.read_csv("s02.txt")
-    data = data.to_numpy()
-    print("oi",data)
+    data = data.iloc[:,:10].to_numpy()
+    
+    model = kmeans(data,4)
+    model.simulate()
+    model.view_clusters()
+
+main()
